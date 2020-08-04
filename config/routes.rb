7d5d94bb9d5f4f6fa517c93d0c0ad1ce39894f53
github username <github email address>
2020-08-04 17:30:11 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+
+  get "/students", to: 'students#index', as: 'movies'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
